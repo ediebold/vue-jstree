182e9c282bd82ab7d4ac200ed3b8358be9e17781
ELLIOT DIEBOLD <>
2018-05-21 10:49:33 +1000

    actually made demo run in dev (will undo this commit later probably)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 /**
  * Created by virus_zhh on 2017/9/29.
  */
+// import VJstree from './tree.vue'
+
+// VJstree.install = function(Vue){
+//   Vue.component(VJstree.name, VJstree)
+// }
+
+// if (typeof window !== 'undefined' && window.Vue) {
+//   window.Vue.use(VJstree);
+// }
+
+// export default VJstree
+
+import Vue from 'vue'
+
+import App from '../App.vue'
+
 import VJstree from './tree.vue'
+Vue.component(VJstree.name, VJstree)
 
-VJstree.install = function(Vue){
-  Vue.component(VJstree.name, VJstree)
-}
 
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(VJstree);
-}
+Vue.config.productionTip = false
 
-export default VJstree
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,9 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: true,
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
   },
   performance: {
     hints: false
